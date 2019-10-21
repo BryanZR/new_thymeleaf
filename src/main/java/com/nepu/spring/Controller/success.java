7cab88db5b4ac7c62436bd25374513a65024722f
bryan <zr_bryan@163.com>
2019-10-21 19:41:16 +0800

    tow commit

--- src/main/java/com/nepu/spring/Controller/success.java
+++ src/main/java/com/nepu/spring/Controller/success.java
@@ -1,8 +1,13 @@
 package com.nepu.spring.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
 
 @Controller
 public class success {
@@ -13,8 +18,11 @@ public class success {
         return "你好！！！";
     }
     @RequestMapping("success")
-    public  String success(){
+    public  String success(Map<String,Object> map) {
+        map.put("hello", "<h1>我是你你爹</h1>");
+        map.put("user", Arrays.asList("zhngsan","lisi","wangwyu"));
         return "success";
+
     }
 
 }
