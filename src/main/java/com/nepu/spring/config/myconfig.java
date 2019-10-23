c2d33d59b9c45e24a3b878adccf66b7024b8e86b
bryan <zr_bryan@163.com>
2019-10-23 11:11:07 +0800

    properties commit

--- src/main/java/com/nepu/spring/config/myconfig.java
+++ src/main/java/com/nepu/spring/config/myconfig.java
@@ -2,16 +2,38 @@ package com.nepu.spring.config;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.*;
+
 //@EnableWebMvc
 @Configuration
 public class myconfig implements WebMvcConfigurer {
-    @Override
+   /* @Override
     public void addViewControllers(ViewControllerRegistry registry) {
 registry.addViewController("/").setViewName("index");
+    }*/
+    //所有的webmvccof都会
+    @Bean
+    public WebMvcConfigurerAdapter  webMvcConfigurerAdapter (){
+        WebMvcConfigurerAdapter adapter = new WebMvcConfigurerAdapter() {
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/").setViewName("login");
+                registry.addViewController("/login.html").setViewName("login");
+            }
+        };
+        return adapter;
     }
+
+    @Configuration
+    public class PathConfig extends WebMvcConfigurerAdapter {
+//        重写addResourceHandlers（）后，\upload\ 下的静态资源可以访问了
+        @Override
+        public void addResourceHandlers(ResourceHandlerRegistry registry) {
+            registry.addResourceHandler("/upload/**").addResourceLocations("classpath:/upload/");
+            super.addResourceHandlers(registry);
+        }
+    }
+
 }
